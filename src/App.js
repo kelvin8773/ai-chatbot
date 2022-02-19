b1ee851826ae622ec60bc5a922b63f7ce7aa724c
Kelvin Liang <kelvin9877@gmail.com>
2022-02-19 22:48:13 +0800

    remove unnecessary files and code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import Chatbot from 'react-chatbot-kit'
+import Chatbot from 'react-chatbot-kit';
+import 'react-chatbot-kit/build/main.css';
+
 import MessageParser from './MessageParser';
 import ActionProvider from './ActionProvider';
-import config from "./config";
-import 'react-chatbot-kit/build/main.css';
+import config from './config';
+
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Chatbot 
-          config={config} 
-          actionProvider={ActionProvider} 
+    <div className='App'>
+      <header className='App-header'>
+        <Chatbot
+          config={config}
+          actionProvider={ActionProvider}
           messageParser={MessageParser}
           headerText='Chatbot'
           placeholderText='Input placeholder'
-          />
+        />
       </header>
     </div>
   );
